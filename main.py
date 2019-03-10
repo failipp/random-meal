1371d57e917e864f475a0833a49d86942dfbdcf6
failipp <p.scheede@gmx.de>
2019-03-11 00:11:42 +0100

    First functioning version of flask interface
    Added basic functionality via flask. Can read meals from data.json.
    Will suggest three possible meals from beginning of the list
    and you can choose one. This one will be moved to the end of the list.
    
    Doesn't support saving the last sequence of meals.

--- main.py
+++ main.py
@@ -2,13 +2,39 @@
 
 import json
 import random
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
 
 from lib import Meal, Mealhandler
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/chose_meal")
+def startpage():
+    global ms
+    grenze = len(ms) // 2
+    ml = list(enumerate(ms[:grenze]))
+    print(ml)
+    random.shuffle(ml)
+    res = ml[:3]
+    return render_template('main.html', meals=res)
+
+
+@app.route("/eat_meal/<int:meal>/", methods=["post"])
+def eat_meal(meal):
+    global ms
+    eaten = ms.pop(meal)
+    ms.append(eaten)
+    return redirect(url_for('startpage'))
+
+
+if __name__ == '__main__':
+    global ms
     with open("data.json", "r") as f:
         me = f.read()
 
@@ -18,26 +44,4 @@ if __name__ == "__main__":
     # ms = [Meal.decode(x) for x in data]
     ms = mH._meals
 
-    print(ms)
-
-    try:
-        while True:
-            grenze = len(ms) // 2
-            rint = random.randint(0, grenze)
-
-            zu_kochen = ms.pop(rint)
-
-            print(zu_kochen)
-            ms.append(zu_kochen)
-            print(ms, end="")
-
-            input()
-    except KeyboardInterrupt:
-        print("Loop End")
-
-    to_write = json.dumps(mH.export(),
-                          ensure_ascii=False,
-                          indent=4)
-
-    with open("data.json", "w") as f:
-        f.write(to_write)
+    app.run(debug=True)
