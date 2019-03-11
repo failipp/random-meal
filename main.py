f12562a0be58c5a0c85c3a854ce03ef3ea9d0a21
failipp <p.scheede@gmx.de>
2019-03-11 15:21:59 +0100

    Migrate from data.json to mongodb
    Meals and the order they were last eaten in are now read from mongodb.

--- main.py
+++ main.py
@@ -3,10 +3,14 @@
 import json
 import random
 from flask import Flask, redirect, render_template, url_for
+from pymongo import MongoClient
 
 from lib import Meal, Mealhandler
 
 app = Flask(__name__)
+database = MongoClient()["random_meal_generator"]
+MEALS = database["meals"]
+MEAL_SEQUENCE = database["meal_sequence"]
 
 
 @app.route("/")
@@ -16,32 +20,38 @@ def index():
 
 @app.route("/chose_meal")
 def startpage():
-    global ms
+    ms = MEAL_SEQUENCE.find_one({"_id": 0})["meal_sequence"]
     grenze = len(ms) // 2
-    ml = list(enumerate(ms[:grenze]))
+    ml = ms[:grenze]
     print(ml)
     random.shuffle(ml)
     res = ml[:3]
-    return render_template('main.html', meals=res)
+    meals = [
+        (ml[0], MEALS.find_one({"_id": ml[0]})["name"]),
+        (ml[1], MEALS.find_one({"_id": ml[1]})["name"]),
+        (ml[2], MEALS.find_one({"_id": ml[2]})["name"]),
+    ]
+    return render_template('main.html', meals=meals)
 
 
-@app.route("/eat_meal/<int:meal>/", methods=["post"])
+@app.route("/eat_meal/<meal>/", methods=["post"])
 def eat_meal(meal):
-    global ms
-    eaten = ms.pop(meal)
+    ms = MEAL_SEQUENCE.find_one({"_id": 0})["meal_sequence"]
+    eaten = ms.pop(ms.index(meal))
     ms.append(eaten)
+    MEAL_SEQUENCE.update_one({"_id": 0}, {"$set": {"meal_sequence": ms}})
     return redirect(url_for('startpage'))
 
 
 if __name__ == '__main__':
-    global ms
-    with open("data.json", "r") as f:
-        me = f.read()
-
-    print(me)
-    data = json.loads(me)
-    mH = Mealhandler(data)
-    # ms = [Meal.decode(x) for x in data]
-    ms = mH._meals
+    # global ms
+    # with open("data.json", "r") as f:
+    #     me = f.read()
+    #
+    # print(me)
+    # data = json.loads(me)
+    # mH = Mealhandler(data)
+    # # ms = [Meal.decode(x) for x in data]
+    # ms = mH._meals
 
     app.run(debug=True)
